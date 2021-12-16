c2085c88d1562e3483aa2b850eb68f6da5d66ebc
Carlotta Chen <carlottachen92@gmail.com>
2021-12-16 13:26:39 -0800

    Initial commit

---
+++ public/function.js
@@ -0,0 +1,10 @@
+const button = document.querySelector('button');
+
+function randomNumber(){
+	let max = 100;
+	let result = Math.floor(Math.random() * max);
+	alert(result);
+}
+button.addEventListener('click', randomNumber);
+
+
---
+++ server/index.js
@@ -0,0 +1,20 @@
+//server file
+
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '../index.html'));
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../functions.js'));
+})
+
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'));
+})
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => { console.log(`Working on PORT ${port}`) });
